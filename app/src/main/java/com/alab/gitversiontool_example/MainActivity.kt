8aa00a4d7594b18f23507587e76ae581c5eb5785
ALab <a.lab.develop@mail.ru>
2022-03-19 15:24:33 +0300

    Feature 2

--- app/src/main/java/com/alab/gitversiontool_example/MainActivity.kt
+++ app/src/main/java/com/alab/gitversiontool_example/MainActivity.kt
@@ -12,5 +12,6 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<TextView>(R.id.textView).text = "Версия: ${BuildConfig.VERSION_NAME}"
         Log.d("myL", "1")
+        Log.d("myL", "2")
     }
 }
